467c13acb3cfee1bc7d1ab4d772808ae712f71d1
David <auscompgeek@users.noreply.github.com>
2015-08-22 18:11:24 +1000

    grok: slight cleanup, improve wording of message

--- plugins/web/grok.py
+++ plugins/web/grok.py
@@ -3,11 +3,12 @@ Polls the grok forums for new posts
 """
 from dateutil.parser import parse as parse_date
 from dateutil.relativedelta import relativedelta
-from datetime import tzinfo, timedelta
+#from datetime import tzinfo, timedelta
 import requests
 import re
 
 DISCOURSE_FORUM_BASE = 'https://forum.groklearning.com'
+
 
 class Plugin(object):
     def load(self, bot, config):
@@ -20,14 +21,13 @@ class Plugin(object):
         self.categoryNames = {}
         self.seenPosts = {}
         self.loaded = False
-        print(config)
         self._loadCategories()
         self._resetSeenPosts()
 
     def _getHeaders(self):
         return {
-                'cookie': self.cookieString,
-                'user-agent': 'ninjabot web.grok (like Gecko; rv:1.0) AppleWebKit/367.5 Gecko/20100101 Firefox/41.0'
+            'cookie': self.cookieString,
+            'user-agent': 'ninjabot web.grok (like Gecko; rv:1.0) AppleWebKit/367.5 Gecko/20100101 Firefox/41.0'
         }
 
     def _loadCategories(self):
@@ -56,18 +56,26 @@ class Plugin(object):
         self._forEachLatestPost(lambda post: self.seenPosts.update({self._getPostUUID(post): True}))
         self.loaded = True
 
-    def getPostMsg(self,json):
-        is_new = json['posts_count'] == 1
+    def getPostMsg(self, json):
+        def pluralise(num, singular, plural=None):
+            if not plural:
+                plural = singular + 's'
+            if num == 1:
+                return '1 ' + singular
+            return '{} {}'.format(num, plural)
+
+        post_num = json['posts_count']
+        is_new = post_num == 1
         time = parse_date(json['created_at'] if is_new else json['last_posted_at']) + relativedelta(hours=14)
-        print(time)
         user = json['last_poster_username'][0] + '\u200D' + json['last_poster_username'][1:]
-        url = 'https://forum.groklearning.com/t/-/{0}/{1}'.format(json['id'], json['posts_count'])
-        viewDesc = str(json['views']) + ' view' + ('s' if json['views'] != 1 else '')
-        likeDesc = str(json['like_count']) + ' like' + ('s' if json['like_count'] != 1 else '')
-        postDesc = str(json['posts_count']) + ' post' + ('s' if json['posts_count'] != 1 else '')
-        res = "'\x02{title}\x0F' in {cat} at \x02{time}\x0F. {posts}, most recent by @\x02{user}\x0F. {likes}, {views}. \x02{url}\x0F"
+        url = 'https://forum.groklearning.com/t/-/{0}/{1}'.format(json['id'], post_num)
+        viewDesc = pluralise(json['views'], 'view')
+        likeDesc = pluralise(json['like_count'], 'like')
+        postDesc = pluralise(post_num, 'post')
+        res = "@\x02{user}\x0F posted in '\x02{title}\x0F' in {cat} at \x02{time}\x0F. {posts}, {likes}, {views}. {url}"
         if is_new:
-            res = "'\x02{title}\x0F' in {cat} at \x02{time}\x0F by @\x02{user}\x0F. {views}. \x02{url}\x0F"
+            res = "New forum topic! '\x02{title}\x0F' in {cat} at \x02{time}\x0F by @\x02{user}\x0F. {views}. {url}"
+
         return res.format(
                     title=json['title'],
                     cat=self.categoryNames[json['category_id']],
@@ -85,19 +93,17 @@ class Plugin(object):
 #        self.bot.privmsg(msg.channel, "Polled")
 
     def timer_60(self):
-        if not self.loaded: return
-        #print("Polling...")
+        if not self.loaded:
+            return
+
         def _onPost(json):
-            if json['category_id'] not in self.categoryIDs: return
-            if json['id'] in self.ignoreThreads: return
-            if self._getPostUUID(json) in self.seenPosts: return
+            if json['category_id'] not in self.categoryIDs:
+                return
+            if json['id'] in self.ignoreThreads:
+                return
+            if self._getPostUUID(json) in self.seenPosts:
+                return
             self.seenPosts[self._getPostUUID(json)] = True
-            if json['posts_count'] == 1:
-                self.bot.privmsg(self.notifyChan, "New forum topic! {0}".format(self.getPostMsg(json)))
-            else:
-                self.bot.privmsg(self.notifyChan, "New forum post in {0}".format(self.getPostMsg(json)))
+            self.bot.privmsg(self.notifyChan, self.getPostMsg(json))
 
         self._forEachLatestPost(_onPost)
-        #print("Done.")
-
-
